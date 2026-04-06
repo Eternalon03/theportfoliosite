a010d89e116dcce344c35718b22e1241191e354c
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2026-04-05 20:34:44 -0400

    mobile resizing of hero section

--- src/app/components/home/about-me/index.tsx
+++ src/app/components/home/about-me/index.tsx
@@ -14,11 +14,11 @@ const AboutMe = () => {
                     <FutureHeader level={2} text="About Me" color="var(--medium-blue)"/>
                     
                     <p className={styles.aboutDescription}>
-                        Hi, I'm Nicole! I'm a Toronto-based Software Engineer currently specializing in <span className={styles.highlight}>mobile development at Capital One</span>. My career path has been pretty diverse: having worn many different hats over the years, I've built a versatile skillset that allows me to bridge gaps between roles and adapt quickly.
+                        Hi, I'm Nicole! I'm a Toronto-based Software Engineer currently specializing in <span className={styles.highlight}>mobile development at Capital One</span>. I've worn many different hats over the years, building a versatile skillset that allows me to bridge gaps between roles.
                     </p>
                     
                     <p className={styles.aboutDescription}>
-                        Ultimately, I find that I just like building, and feeling things click. I like being involved in planning and events around my communities, whether that be my work, my city, or my art. Feel free to contact me if you want to plan something!
+                        I like being involved in planning and events around my communities, whether that be my work, my city, or my art. Feel free to contact me if you want to plan something!
                     </p>
 
                     {/*Added a wrapper div to push it to the center */}
